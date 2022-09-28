100e1d61277affd458a4812bcc7c2d89c32f0065
imjcervantes <imjcervantes@gmail.com>
2022-09-28 01:00:09 -0600

    Añadiendo useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
-  const [pacientes, setPacientes] = useState([])
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
 
   return (   
       <div className="container mx-auto mt-1">
@@ -14,8 +15,12 @@ function App() {
           <Formulario 
             pacientes={pacientes}
             setPacientes={setPacientes}
+            paciente={paciente}
           />
-          <ListadoPacientes />
+          <ListadoPacientes 
+            pacientes={pacientes}
+            setPaciente={setPaciente}
+          />
         </div>
       </div>
   )
